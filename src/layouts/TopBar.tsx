65052c4344465d1207fd45bd8b65c4cbc12ba403
NewRenTL <diego.bustamante@utec.edu.pe>
2024-07-19 19:12:50 -0500

    fix(topbar Fixed): position fixed topBar resolved

--- src/layouts/TopBar.tsx
+++ src/layouts/TopBar.tsx
@@ -10,7 +10,7 @@ const TopBar = () => {
 
   return (
     <>
-      <div className="bg-transparent w-full sticky flex flex-col items-center px-5 py-10">
+      <div className="bg-transparent w-full sticky top-0 flex flex-col items-center px-5 py-10">
         <nav className="w-[95%] h-15 flex flex-row rounded-3xl items-center justify-center px-4 py-2 bg-[#FFBC00]">
           <div className="w-[10%] h-[100%] items-center cursor-pointer flex flex-row">
             <img
